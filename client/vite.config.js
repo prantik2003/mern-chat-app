fc20768082627620b334432e59f4f4c3ea4fbe84
Prantik Guin <145896649+prantik2003@users.noreply.github.com>
2024-11-07 18:09:11 +0530

    updates

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port:3000,
     proxy:{
       '/api':{
-        target:'https://mern-chat-app-self-seven.vercel.app',
+        target:'http://localhost:5000',
         
       }
     }
